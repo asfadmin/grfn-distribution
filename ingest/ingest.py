31d4d225f9d68e938de04d2980265f0fa63ad3a0
Andrew Johnston <asjohnston@alaska.edu>
2016-11-02 22:34:31 +0000

    working copy of ingest.py that just moved files from landing bucket to content bucket

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import boto3
 import zipfile
 import os.path
@@ -10,20 +12,16 @@ working_directory = '/tmp/'
 s3 = boto3.resource('s3')
 
 
-def process_object(s3_object):
-    #sanity check
-    local_zip_file = os.path.join(working_directory, s3_object.key)
-    s3_object.download_file(local_zip_file)
-    with zipfile.ZipFile(local_zip_file, 'r') as zip_ref:
-        zip_ref.extractall()
-    # reorganize
-    # zip
-    distribution_zip = local_zip_file
-    s3.Bucket(content_bucket_name).upload_file(distribution_zip)
-    #s3_object.delete()
+def process_object(s3_object_summary):
+    s3_object = s3.Object(s3_object_summary.bucket_name, s3_object_summary.key)
+    local_file = os.path.join(working_directory, s3_object.key)
+    s3_object.download_file(local_file)
+    s3.Bucket(content_bucket_name).upload_file(local_file, s3_object.key)
+    s3_object.delete()
+    os.remove(local_file)
 
 
 landing_bucket = s3.Bucket(landing_bucket_name)
-for object in landing_bucket.objects.all():
-    process_object(object)
+for object_summary in landing_bucket.objects.all():
+    process_object(object_summary)
 
