bb9b7477ced90bdef969aa51d787c27c9e10eb65
Andrew Johnston <asjohnston@alaska.edu>
2016-11-03 18:39:18 +0000

    GRFN-22: refactor genindex.py to use jinja2 template engine

--- door/genindex.py
+++ door/genindex.py
@@ -1,38 +1,43 @@
+#!/usr/bin/python
+
 import boto3
+from jinja2 import Template
 
 bucket_name = 'grfn-d-fe9f1b34-1425-56b9-939f-5f1431a6d1de'
 expire_time_in_seconds = 86400
 output_html_name = '/var/www/html/index.html'
+template_file = 'index.html.template'
 
-license_name = '/var/www/html/sentinel_terms.txt'
-with open(license_name, 'r') as st:
-   eula_text = st.read()
 
-s3_client = boto3.client('s3')
-bucket = boto3.resource('s3').Bucket(bucket_name)
+def get_links():
+    s3_client = boto3.client('s3')
+    bucket = boto3.resource('s3').Bucket(bucket_name)
 
-files = []
-for obj in bucket.objects.all():
-    url = s3_client.generate_presigned_url(
-        ClientMethod='get_object',
-        Params = {
-             'Bucket': obj.bucket_name,
-             'Key': obj.key,
-        },
-        ExpiresIn = expire_time_in_seconds
-    )
-    files.append({'key': obj.key, 'url': url})
+    links = []
+    for obj in bucket.objects.all():
+        url = s3_client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params = {
+                 'Bucket': obj.bucket_name,
+                 'Key': obj.key,
+            },
+            ExpiresIn = expire_time_in_seconds
+        )
+        links.append({'text': obj.key, 'url': url})
+    return links
 
-with open(output_html_name, 'w') as f:
-    f.write('<html>\n')
-    f.write('<head>\n')
-    f.write('</head>\n')
-    f.write('<body>\n')
-    f.write('<pre>\n')
-    f.write(eula_text)   
-    f.write('</pre>\n')
-    for file in files:
-        f.write('<a href="{0}">{1}</a>\n'.format(file['url'], file['key']))
-        f.write('<br />\n')
-    f.write('</body>\n')
-    f.write('</html>\n')
+
+def get_content():
+    with open(template_file, 'r') as t:
+        template_text = t.read()
+    links = get_links()
+    template = Template(template_text)
+    content = template.render(links=links)
+    return content
+
+
+if __name__ == "__main__":
+    content = get_content()
+    with open(output_html_name, 'w') as f:
+        f.write(content)
+
