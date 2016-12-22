0bf23e4f7985619a2ba29da79f3222f8dd3fd6f4
Andrew Johnston <asjohnston@alaska.edu>
2016-12-22 23:15:45 +0000

    trigger cmr reporting pipeline when specified in configuration for output files

--- ingest/ingest.py
+++ ingest/ingest.py
@@ -8,8 +8,44 @@ import yaml
 import argparse
 import asf.log
 import logging
+import re
+import json
 
 log = logging.getLogger()
+
+
+def get_isce_data(source_zip):
+    source_dir = os.path.splitext(source_zip)[0]
+    with zipfile.ZipFile(source_zip, 'r') as z:
+        source_file = os.path.join(source_dir, 'isce.log')
+        content = z.read(source_file)
+    m = re.search('ctx: ({.*})\n\d', content, re.DOTALL)
+    data = json.loads(m.group(1))
+    data['bounding_box'] = {}
+    for line in content.split('\n'):
+        pattern = 'geocode.(North|South|East|West) = (.*)$'
+        match = re.search(pattern, line)
+        if match is not None:
+            data['bounding_box'][match.group(1).lower()] = match.group(2)
+    return data
+
+
+def get_cmr_payload(source_zip, file_name, collection):
+    isce_data = get_isce_data(source_zip)
+    payload = {
+        'file_name': file_name,
+        'collection': collection,
+        'master_granule_ur': os.path.splitext(min(isce_data['master_zip_file']))[0] + '-SLC',
+        'slave_granule_ur': os.path.splitext(max(isce_data['slave_zip_file']))[0] + '-SLC',
+        'file_size': os.path.getsize(file_name),
+        'bounding_box': isce_data['bounding_box'],
+    }
+    return payload
+
+
+def send_to_cmr(cmr_config, payload):
+    print(payload)
+    boto3.client('lambda').invoke(FunctionName=cmr_config['lambda_name'], InvocationType='Event', Payload=json.dumps(payload), Qualifier=cmr_config['lambda_alias'])
 
 
 def create_output_zip(source_name, dest_name, files):
@@ -30,19 +66,22 @@ def upload_object(bucket_name, key, local_file_name):
     bucket.upload_file(local_file_name, key)
 
 
-def process_output_file(output_file_config, input_file_name, content_bucket_name):
+def process_output_file(output_file_config, input_file_name, content_bucket_name, cmr_config):
     output_file_name = os.path.splitext(input_file_name)[0] + output_file_config['extension']
     log.info('Processing output file {0}'.format(output_file_name))
     create_output_zip(input_file_name, output_file_name, output_file_config['files'])
     upload_object(content_bucket_name, os.path.split(output_file_name)[1], output_file_name)
+    if 'cmr_collection' in output_file_config:
+        cmr_payload = get_cmr_payload(input_file_name, output_file_name, output_file_config['cmr_collection'])
+        send_to_cmr(cmr_config, cmr_payload)
     os.remove(output_file_name)
     log.info('Done processing output file {0}'.format(output_file_name))
 
 
-def ingest_object(obj, content_bucket_name, output_file_configs):
+def ingest_object(obj, content_bucket_name, output_file_configs, cmr_config):
     obj.download_file(obj.key)
     for output_file_config in output_file_configs:
-        process_output_file(output_file_config, obj.key, content_bucket_name)
+        process_output_file(output_file_config, obj.key, content_bucket_name, cmr_config)
     upload_object(content_bucket_name, obj.key, obj.key)
     os.remove(obj.key)
     obj.delete()
@@ -83,7 +122,7 @@ def ingest_loop(ingest_config):
         obj = get_object_to_ingest(ingest_config['landing_bucket_name'])
         if obj:
             log.info('Processing input file {0}'.format(obj.key))
-            ingest_object(obj, ingest_config['content_bucket_name'], ingest_config['output_files'])
+            ingest_object(obj, ingest_config['content_bucket_name'], ingest_config['output_files'], ingest_config['cmr'])
             log.info('Done processing input file {0}'.format(obj.key))
         else:
             time.sleep(ingest_config['sleep_time_in_seconds'])
