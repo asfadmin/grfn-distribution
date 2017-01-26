576be3c4f98a7ef513e9ae650637737cd21e959f
Brian Buechler <btbuechler@alaska.edu>
2017-01-26 18:46:04 +0000

    [GRFN-199] Add a delay between queue purge and population

--- utility/reset_ingest_queue.py
+++ utility/reset_ingest_queue.py
@@ -1,5 +1,6 @@
 import boto3
 import json
+import time
 from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
@@ -15,14 +16,19 @@ def lambda_handler(event, context):
 
        # Theres really no better way to do this?
        try:
+          print ("Beginning queue purge")
           queueUrl = sqsc.get_queue_url(QueueName=event["reset_queue"])['QueueUrl']
           sqsc.purge_queue(QueueUrl=queueUrl)
+          time.sleep(60)
+          print ("Completed queue purge")
+ 
        except ClientError as e:
           if 'PurgeQueueInProgress' in e.response['Error']['Code']:
              print ("Cannot reset queue, purge in progress. Try again later")
              return False
           raise (e)
 
+       q_cnt = 0
        for item in s3.Bucket(event["reset_bucket"]).objects.all():
           if not item.key.endswith(".zip"):
              continue
@@ -31,8 +37,9 @@ def lambda_handler(event, context):
                                         "object": { "key": item.key}}}]} 
           response = queue.send_message(MessageBody=json.dumps(msg))
           print ("Added queue entry for {0}".format(item.key))
+          q_cnt += 1
        
-       return True
+       return "Queued {0} items".format(q_cnt)
       
     return False
 
