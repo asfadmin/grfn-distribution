fce3ba4a83a5e37dbb68c509c659a7345d518457
Andrew Johnston <asjohnston@alaska.edu>
2018-02-20 23:00:02 +0000

    first draft of removing index page

--- src/door/door.py
+++ src/door/door.py
@@ -4,7 +4,7 @@ import os
 import json
 from flask import Flask, g, redirect, render_template, request, abort
 from botocore.exceptions import ClientError
- 
+
 app = Flask(__name__)
 
 
@@ -14,16 +14,12 @@ def init_app():
         path_parts = get_environ_value('DOOR_CONFIG').split('/')
         config_body = get_object_body(path_parts[2], '/'.join(path_parts[3:]))
         config = yaml.load(config_body)
-    else: 
+    else:
         with open(get_environ_value('DOOR_CONFIG'), 'r') as f:
             config = yaml.load(f)
     app.config.update(dict(config))
     boto3.setup_default_session(region_name=config['aws_region'])
 
-
-@app.route('/')
-def show_index():
-   return get_content()
 
 @app.route('/userprofile', methods=['GET','POST'])
 def user_profile():
@@ -38,17 +34,18 @@ def user_profile():
       # Leaving this lean as we only have one checkbox
       put_user_preference(table,1,user_id)
       g.perf = "checked"
-    return render_template('userprofile.html'), 200   
-  
+    return render_template('userprofile.html'), 200
+
   response = get_user_preference(table,user_id)
   if response is None:
     put_user_preference(table,1,user_id)
     g.perf = "checked"
   else:
     if response is True:
-      g.perf = "checked"  
-      
+      g.perf = "checked"
+
   return render_template('userprofile.html'), 200
+
 
 @app.route('/download/<file_name>')
 def download_redirect(file_name):
@@ -59,7 +56,7 @@ def download_redirect(file_name):
         if e.response['Error']['Code'] == '404':
             abort(404)
         raise
-  
+
     available = process_availability(obj, app.config['glacier_retrieval'])
 
     if available:
@@ -80,6 +77,7 @@ def get_object(bucket, key):
     obj.load()
     return obj
 
+
 def get_object_body(bucket, key):
     obj = get_object(bucket, key)
     response = obj.get()
@@ -94,7 +92,7 @@ def process_availability(obj, retrieval_opts):
             available = False
         if restore_status in ['not_available', 'available']:  # restoring available objects extends their expiration date
             queue_restore_request(obj)
-                
+
     return available
 
 
@@ -105,26 +103,12 @@ def translate_restore_status(restore):
         return 'in_progress'
     return 'available'
 
+
 def queue_restore_request(obj):
     sqs = boto3.resource('sqs')
     queue = sqs.get_queue_by_name(QueueName=app.config['glacier_restore_sqs'])
     response = queue.send_message(MessageBody=json.dumps({'Bucket':obj.bucket_name, 'Key':obj.key}))
 
-def restore_object(obj, days, tier):
-    try: 
-        obj.restore_object(
-            RestoreRequest = {
-                'Days': days,
-                'GlacierJobParameters': {
-                    'Tier': tier,
-                },
-            }
-        ) 
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'GlacierExpeditedRetrievalNotAvailable':
-            queue_restore_request( obj )
-        else: 
-            raise
 
 def put_user_preference(table, pref, user_name):
     dynamodb = boto3.client('dynamodb')
@@ -134,7 +118,7 @@ def put_user_preference(table, pref, user_name):
         TableName=table,
         Key=primary_key,
         UpdateExpression='set email =  :1',
-        ExpressionAttributeValues={':1': val},  
+        ExpressionAttributeValues={':1': val},
     )
 
 
@@ -145,10 +129,11 @@ def get_user_preference(table, user_name):
     TableName=table,
     Key=primary_key,
     )
-    
-    if 'Item' not in response:	
+
+    if 'Item' not in response:
 	return None
     return response['Item']['email']['BOOL']
+
 
 def log_restore_request(table, obj, email_address):
     dynamodb = boto3.client('dynamodb')
@@ -161,11 +146,10 @@ def log_restore_request(table, obj, email_address):
         ExpressionAttributeValues={':1': val},
     )
 
-        
-def get_environ_value(key):
 
+def get_environ_value(key):
     if key in request.environ:
-        return request.environ.get(key) 
+        return request.environ.get(key)
     elif key in os.environ:
         return os.environ[key]
     else:
@@ -184,20 +168,3 @@ def get_link(bucket_name, object_key, expire_time_in_seconds):
        ExpiresIn = expire_time_in_seconds
     )
     return url
-
-
-def get_objects(bucket_name):
-    s3_client = boto3.client('s3')
-    bucket = boto3.resource('s3').Bucket(bucket_name)
-    
-    object_keys = []
-    for obj in bucket.objects.all():
-       object_keys.append({'text': obj.key})
-
-    return object_keys 
-
-
-def get_content():
-    g.objects = get_objects(app.config['bucket_name'])
-    content = render_template('index.html')
-    return content
