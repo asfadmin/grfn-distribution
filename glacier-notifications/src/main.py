8a35bf9b58488d81d5c855eb0edcbee9f5851e09
Andrew Johnston <asjohnston@alaska.edu>
2018-03-21 18:49:46 +0000

    GRFN-741: untested draft of pulling work from sqs queue

--- glacier-notifications/src/main.py
+++ glacier-notifications/src/main.py
@@ -8,6 +8,8 @@ from jinja2 import Template
 
 log = getLogger()
 ses = boto3.client('ses')
+sqs = boto3.resource('sqs')
+dynamodb = boto3.client('dynamodb')
 
 
 def setup():
@@ -15,12 +17,6 @@ def setup():
     log.setLevel(config['log_level'])
     log.debug('Config: %s', str(config))
     return config
-
-
-def get_restore_requests(table_name):
-    dynamodb = boto3.client('dynamodb')
-    response = dynamodb.scan(TableName=table_name)
-    return response['Items']
 
 
 def build_acknowledgement_email_body(config):
@@ -31,14 +27,35 @@ def build_acknowledgement_email_body(config):
     return email_body
 
 
-def send_acknowledgement_email(to_email, config):
-    ses_message = build_acknowledgement_email(to_email, config)
-    ses.send_email(**ses_message)
+def get_user(user_id, table):
+    results = dynamodb.get_item(
+        TableName=table,
+        Key={'user_id': {'S': user_id}},
+        ProjectionExpression='email_address, last_acknowledgement',
+    )
+    if 'Item' not in results:
+        return None
+
+    item = results['Item']
+    user = {
+        'email_address': item['email_address']['S'],
+    }
+    if 'last_acknowledgement' in item:
+        user['last_acknowledged'] = item['last_acknowledgement']['S']
+    return user
+
+
+def send_acknowledgement_email(data, config):
+    user = get_user(data['user_id'], config['users_table'])
+    if True: #TODO not 'last_acknowledgement' not in user or user['last_acknowledgement'] < datetime.utcnow() - config['frequency']:
+        ses_message = build_acknowledgement_email(user['email_address'], config)
+        ses.send_email(**ses_message)
+    #TODO log if we sent anything
     #TODO update last acknowledged date for user
 
 
 def build_acknowledgement_email(to_email, config):
-    today = date.strftime(datetime.utcnow(), '%B %d, %Y') #TODO deal with time zones
+    today = date.strftime(datetime.utcnow(), '%B %d, %Y')
     subject = 'SAR Products Requested {0} UTC'.format(today)
     email_body = build_acknowledgement_email_body(config['email_body'])
 
@@ -61,35 +78,27 @@ def build_acknowledgement_email(to_email, config):
     return ses_message
 
 
-def process_restore_notifications(config):
-    emails = {}
-    for request in get_restore_requests(config['db_table']):
-        obj = get_object(request)
-        addresses = request['email_addresses']['SS']
-        if is_old(request, 24):
-            delete_request_record(config['db_table'], request)
-            continue
-        for address in addresses:
-            if not address in emails:
-                emails[address] = { 'new': [], 'old': [], 'wait': [] }
-        if is_new(request):
-            if is_available(obj):
-                put_restore_date(config['db_table'], request)
-                for address in addresses:
-                    emails[address]['new'].append( request )
-            else:
-                for address in addresses:
-                    emails[address]['wait'].append( request )
-        else:
-            for address in addresses:
-                emails[address]['old'].append( request )
+def process_sqs_message(sqs_message, config):
+    payload = json.loads(sqs_message.body)
+    if payload['type'] == 'acknowledgement':
+        send_acknowledgement_email(payload['data'], config['email_content'])
+    #TODO trap errors
 
-    for email in emails:
-        if emails[email]['new']:
-            log.warning('Sending notifications to %s', email)
-            send_notification_for_file(email, emails[email], config['email'])
+
+def process_notifications(config):
+    queue = sqs.Queue(config['email_queue_url'])
+
+    while True:
+        messages = queue.receive_messages(MaxNumberOfMessages=config['max_messages_per_receive'], WaitTimeSeconds=config['wait_time_in_seconds'])
+        if not messages:
+            log.info('No messages found.  Exiting.')
+            break
+
+        for sqs_message in messages:
+            process_sqs_message(sqs_message, config['email_content'])
+            sqs_message.delete()
 
 
 def lambda_handler(event, context):
     config = setup()
-    process_restore_notifications(config['restore_notifications'])
+    process_notifications(config['notifications'])
