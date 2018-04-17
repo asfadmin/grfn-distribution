7b28d9a0b17dbb2cb40404c39558cd9cdfdb81d0
Andrew Johnston <asjohnston@alaska.edu>
2018-04-17 19:48:29 +0000

    GRFN-815: update glacier-restore to use object-status lambda

--- glacier-restore/src/main.py
+++ glacier-restore/src/main.py
@@ -1,15 +1,14 @@
 import json
-import re
 from os import environ
 from logging import getLogger
-from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
 
 
 log = getLogger()
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 dynamodb = boto3.client('dynamodb')
+lamb = boto3.client('lambda')
 
 
 def setup():
@@ -19,64 +18,58 @@ def setup():
     return config
 
 
-def get_object(bucket, key):
-    obj = s3.Object(bucket, key)
-    obj.load()
-    return obj
-
-
-def get_request_status(restore_string):
-    if restore_string is None:
-        return 'new'
-    if 'ongoing-request="true"' in restore_string:
-        return 'pending'
-    return 'available'
-
-
-def update_object(bundle_id, object_key, request_status, expiration_date, table):
+def update_object(bundle_id, object_key, object_status, table):
     primary_key = {'bundle_id': {'S': bundle_id}, 'object_key': {'S': object_key}}
+
+    update_expression = 'set request_status = :1'
+    expression_attribute_values = {':1': {'S': object_status['status']}}
+
+    if 'expiration_date' in object_status:
+        update_expression += ', expiration_date = :2'
+        expression_attribute_values[':2'] = {'S': object_status['expiration_date']}
+
     dynamodb.update_item(
         TableName=table,
         Key=primary_key,
-        UpdateExpression='set request_status = :1, expiration_date = :2',
-        ExpressionAttributeValues={
-            ':1': {'S': request_status},
-            ':2': {'S': str(expiration_date)},
-        },
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=expression_attribute_values,
     )
 
 
-def get_expiration_date(restore_string):
-    match = re.search('expiry-date="(.+)"', restore_string)
-    if match:
-        return datetime.strptime(match.group(1), '%a, %d %b %Y %H:%M:%S %Z')
-    return None
-
-
-def restore_object(obj, tier, retention_days):
-    log.info('Restoring object.  Object Key: %s, Tier: %s, Retention Days: %s', obj.key, tier, retention_days)
+def restore_object(request, config):
+    tier = request.get('tier', config['default_tier'])
+    log.info('Restoring object.  Object Key: %s, Tier: %s', request['object_key'], tier)
     try:
-        obj.restore_object(
+        s3.restore_object(
+            Bucket=config['bucket'],
+            Key=request['object_key'],
             RestoreRequest={
-                'Days': retention_days,
+                'Days': config['retention_days'],
                 'GlacierJobParameters': {
                     'Tier': tier,
                 },
-            }
+            },
         )
     except ClientError as e:
         log.exception('Failed to restore object.')
 
 
+def get_object_status(object_key, object_status_lambda):
+    payload = {'object_key': object_key}
+    response = lamb.invoke(
+        FunctionName=object_status_lambda,
+        Payload=json.dumps(payload),
+    )
+    object_status = json.loads(response['Payload'].read())
+    return object_status
+
+
 def process_request(request, config):
-    obj = get_object(config['bucket'], request['object_key'])
-    tier = request.get('tier', config['default_tier'])
-    restore_object(obj, tier, config['retention_days'])
+    restore_object(request, config['restore'])
     if 'bundle_id' in request:
-        request_status = get_request_status(obj.restore)
-        if request_status in ['pending', 'available']:
-            expiration_date = get_expiration_date(obj.restore)
-            update_object(request['bundle_id'], request['object_key'], request_status, expiration_date, config['objects_table'])
+        object_status = get_object_status(request['object_key'], config['object_status_lambda'])
+        if object_status['status'] in ['retrieving', 'available']:
+            update_object(request['bundle_id'], request['object_key'], object_status, config['objects_table'])
 
 
 def lambda_handler(event, context):
